b44e74eb9887bd7f1f16c9de01ad849dcfdd0447
Emily Schultz <ess2183@columbia.edu>
2012-11-28 16:52:30 -0500

    Part2a done.

--- http-server.c
+++ http-server.c
@@ -48,10 +48,16 @@ int main(int argc, char **argv)
 
   // parse args
   servPort = atoi(argv[1]); /* second arg: server port */
-  char *webroot = argv[2];
-  // for part 2b
-  char *host = argv[3];
-  unsigned short lookup_port = atoi(argv[4]);  /* fourth arg: lookup port */
+  char *webroot = argv[2];  /* third arg: root */
+
+  char *mdb_host = argv[3];  /*fourth arg: host path */
+  unsigned short mdb_port = atoi(argv[4]);  /* fifth arg: lookup port */
+
+  /* Create socket for mdb-lookup */
+
+
+
+  // if((clntSocket
 
 
   /* Create socket for incoming connections */
@@ -108,39 +114,65 @@ void HandleTCPClient(int clntSocket, char* web_root, char* IPaddr)
       char *requestURI = strtok(NULL, token_separators);
       char *httpVersion = strtok(NULL, token_separators);
 
-      if((strncmp("GET", method, strlen(method)) != 0) || 
-	 (strncmp("HTTP/1.0", httpVersion, 8) != 0 && 
-	  strncmp("HTTP/1.1", httpVersion, 8) != 0))
-	{ // only support GET method, must be either HTTP/1.0 or 1.1
-	  // otherwise send 501 not implemented
-	  fprintf(stdout, "%s \"%s %s %s\" 501 Not Implemented\n", 
-		  IPaddr, method, requestURI, httpVersion);
-	  fflush(stdout);
-	  int length = sprintf(out_buffer, "HTTP/1.0 501 Not Implemented"
-		"\n\n<html><body><h1>501 Not Implemented</h1></body></html>");
-	  if(send(clntSocket, out_buffer, length, 0) != length)
-	    {
-	      perror("send() failed");
-	      fclose(input);
-	    }
-	  fclose(input);
-	}
-      else if(strncmp("/", requestURI, 1) != 0)
+      char *sendHeader = "HTTP/1.0";
+      char *outtext = method;
+
+      if(method == NULL || requestURI == NULL 
+	 || httpVersion == NULL || strncmp("/", requestURI, 1) != 0)
 	{ // request URI must start with /
 	  // otherwise send 400 bad request
-
 	  fprintf(stdout, "%s \"%s %s %s\" 400 Bad Request\n", 
 		  IPaddr, method, requestURI, httpVersion);
 	  fflush(stdout);
-	  int length = sprintf(out_buffer, "HTTP/1.0 400 Bad Request"
-		 "\n\n<html><body><h1>400 Bad Request</h1></body></html>");
+	  int length = sprintf(out_buffer, "HTTP/1.0 400 Bad Request\n"
+		 "\n<html><body><h1>400 Bad Request</h1></body></html>\n");
 	  if(send(clntSocket, out_buffer, length, 0) != length)
 	    {
 	      fclose(input);
 	      die("send() failed");
 	    }
+	  fclose(input);
+	}
+      else if((strcmp("GET", method) != 0) || 
+	 (strcmp("HTTP/1.0", httpVersion) != 0 && 
+	  strcmp("HTTP/1.1", httpVersion) != 0))
+	{ // only support GET method, must be either HTTP/1.0 or 1.1
+	  // otherwise send 501 not implemented
+	  fprintf(stdout, "%s \"%s %s %s\" 501 Not Implemented\n", 
+		  IPaddr, method, requestURI, httpVersion);
+	  fflush(stdout);
+	  int length = sprintf(out_buffer, "HTTP/1.0 501 Not Implemented\n"
+		"\n<html><body><h1>501 Not Implemented</h1></body></html>\n");
+	  if(send(clntSocket, out_buffer, length, 0) != length)
+	    {
+	      fclose(input);
+	      die("send() failed");
+	    }
+	  fclose(input);
 	}
       // successful request, send what they ask for
+      else if(strcmp(requestURI, "/mdb-lookup") == 0)
+	{
+
+	  fprintf(stdout, "%s \"%s %s %s\" 200 OK\n", 
+		  IPaddr, method, requestURI, httpVersion);
+	  fflush(stdout);
+
+	  const char *form = "<html><body><h1>mdb-lookup</h1>\n"
+	    "<p>\n"
+	    "<form method=GET action=/mdb-lookup>\n"
+	    "lookup: <input type=text name=key>\n"
+	    "<input type=submit>\n"
+	    "</form>\n"
+	    "<p></body></html>\n";
+
+	  if(send(clntSocket, form, strlen(form), 0) != strlen(form)) 
+	    {
+	      fclose(input);
+	      die("send() failed");
+	    }
+	  fclose(input);
+	}
       else
 	{
 	  char webfile[strlen(web_root) + strlen(requestURI) + 11];
@@ -157,12 +189,12 @@ void HandleTCPClient(int clntSocket, char* web_root, char* IPaddr)
 	      fprintf(stdout, "%s \"%s %s %s\" 404 Not Found\n", 
 		  IPaddr, method, requestURI, httpVersion);
 	      fflush(stdout);
-	      int length = sprintf(out_buffer, "HTTP/1.0 404 Not Found"
-		 "\n\n<html><body><h1>404 Not Found</h1></body></html>");
+	      int length = sprintf(out_buffer, "HTTP/1.0 404 Not Found\n"
+		 "\n<html><body><h1>404 Not Found</h1></body></html>\n");
 	      if(send(clntSocket, out_buffer, length, 0) != length)
 		{
 		  fclose(input);
-		  die("send() failed");
+		  die("send() here failed");
 		}
 	      fclose(input);
 	    }
@@ -172,6 +204,12 @@ void HandleTCPClient(int clntSocket, char* web_root, char* IPaddr)
 		      IPaddr, method, requestURI, httpVersion);
 	      fflush(stdout);
 	      int length = 0;
+	      char* header = "HTTP/1.0 200 OK\n\n";
+	      if(send(clntSocket, header, strlen(header), 0) != strlen(header))
+		{
+		  fclose(input);
+		  die("header failed");
+		}
 	      while((length = 
 		     fread(out_buffer, 1, sizeof(out_buffer), read_file)) > 0)
 		{
@@ -185,14 +223,10 @@ void HandleTCPClient(int clntSocket, char* web_root, char* IPaddr)
 		      fclose(input);
 		      die("send() failed");
 		    }
-		  memset(out_buffer, '\0', sizeof(out_buffer));
+		  memset(out_buffer, '\0', strlen(out_buffer));
 		}
 	      fclose(input);
 	    }
     	}
     }
-  // CHANGE: check 'input' rather than 'stdin'
-  // see if fgets() produced error
-
 }
-
